4c0531756e6f812c77166f5ea911281b16b57edd
luc1dLife <luc1d_life@protonmail.com>
2020-09-05 18:24:27 -0600

    Linted

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,75 +3,73 @@ var limitWarningEl = document.querySelector("#limit-warning");
 var repoNameEl = document.querySelector("#repo-name");
 
 function getRepoName() {
-	var queryString = document.location.search;
-	var repoName = queryString.split("=")[1];
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
 
-	if (repoName) {
-		getRepoIssues(repoName);
-		repoNameEl.textContent = repoName;
-	} else {
-		document.location.replace("./index.html")
-	}
-
+    if (repoName) {
+        getRepoIssues(repoName);
+        repoNameEl.textContent = repoName;
+    } else {
+        document.location.replace("./index.html");
+    }
 }
 
 function getRepoIssues(repo) {
-	var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
-	fetch(apiUrl).then(function (response) {
-		if (response.ok) {
-			response.json().then(function (data) {
-				displayIssues(data);
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayIssues(data);
 
-				if (response.headers.get("Link")) {
-					displayWarning(repo);
-				}
-			})
-		} else {
-			document.location.replace("./index.html");
-		}
-	});
-};
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
+            });
+        } else {
+            document.location.replace("./index.html");
+        }
+    });
+}
 
 function displayIssues(issues) {
-	if (issues.lenght === 0) {
-		issueContainerEl.textContent = "This repo has no open issues!";
-		return;
-	}
-	for (var i = 0; i < issues.length; i++) {
-		var issueEl = document.createElement("a");
-		issueEl.classList = "list-item flex-row justify-space-between align-center";
-		issueEl.setAttribute("href", issues[i].html_url);
-		issueEl.setAttribute("target", "_blank");
+    if (issues.lenght === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+    }
+    for (var i = 0; i < issues.length; i++) {
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
 
-		var titleEl = document.createElement("span");
-		titleEl.textContent = issues[i].title;
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
 
+        issueEl.appendChild(titleEl);
 
-		issueEl.appendChild(titleEl);
+        var typeEl = document.createElement("span");
 
-		var typeEl = document.createElement("span");
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull Request)";
+        } else {
+            typeEl.textContent = "(Issue)";
+        }
 
-		if (issues[i].pull_request) {
-			typeEl.textContent = "(Pull Request)";
-		} else {
-			typeEl.textContent = "(Issue)";
-		}
+        issueEl.appendChild(typeEl);
 
-		issueEl.appendChild(typeEl);
-
-		issueContainerEl.appendChild(issueEl);
-	}
+        issueContainerEl.appendChild(issueEl);
+    }
 }
 
 function displayWarning(repo) {
-	limitWarningEl.textContent = "To see more than 30 issues, visit ";
-	var linkEl = document.createElement("a");
-	linkEl.textContent = "See More Issues on Github.com";
-	linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
-	linkEl.setAttribute("target", "_blank");
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on Github.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
 
-	limitWarningEl.appendChild(linkEl);
+    limitWarningEl.appendChild(linkEl);
 }
 
 getRepoName();
