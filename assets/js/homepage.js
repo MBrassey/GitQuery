8ac4cc7eca121c8d63255e23d4ee54fead512b30
luc1dLife <luc1d_life@protonmail.com>
2020-09-05 18:19:19 -0600

    Added Buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -39,6 +39,20 @@ var getUserRepos = function (user) {
         });
 };
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayRepos(data.items, language);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    });
+  };
+
 var displayRepos = function (repos, searchTerm) {
     // check if api returned any repos
     if (repos.length === 0) {
